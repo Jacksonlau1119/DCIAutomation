0b9705f8b1278cb2ab9e406c4d76a0d70494eb9b
KenY0722 <ken.l@partsengine.ca>
2023-06-30 11:24:30 -0400

    Update daily_ftp.sh

--- daily_ftp.sh
+++ daily_ftp.sh
@@ -1,1 +1,1 @@
-python planner/main.py --dci_dir ./dci --ftp && python  && python bronze_worker/main.py --dci_dir ./dci
+python planner/main.py --dci_dir ./dci --ftp  && python bronze_worker/main.py --dci_dir ./dci
